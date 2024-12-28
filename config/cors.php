e22c1274775829f0dcab73335af27c737130a1dc
itsmemarkmalvar <149479021+itsmemarkmalvar@users.noreply.github.com>
2024-12-29 00:28:13 +0800

    Actual and Virtual Device Implementation, User Login and Logout Proper Handling

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,19 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => true,
+    'allowed_headers' => [
+        'Content-Type',
+        'X-Requested-With',
+        'Authorization',
+        'Accept',
+        'Origin',
+        'Access-Control-Allow-Origin'
+    ],
+    'exposed_headers' => ['Authorization'],
+    'max_age' => 86400,
+    'supports_credentials' => false,
+    'paths' => ['*'],
 ]; 
